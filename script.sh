048b1171a37c188309814cd7bf6b363c2aa16105
BOSSLADY <gennymiriane06@gmail,com>
2025-11-20 17:13:51 +0100

    a guessing game

--- script.sh
+++ script.sh
@@ -1,1 +1,13 @@
-#!/bin/bash
+#!/bin/bash#!/bin/bash
+x=$(( 0 + RANDOM % (5 - 0 + 1) ))
+echo "=========================================="
+echo "  Guest a number to win between 0 to 5: "
+echo "=========================================="
+ read -p "Enter the value you guest: " y
+if [[ $y -eq $x ]]; then
+  echo "You WIN"
+elif [[ $y -lt $x ]]; then
+   echo "your too low"
+   echo "you below the win number"
+   fi
+
