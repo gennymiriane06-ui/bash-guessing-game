a0c8b514c28aceb5f2434a1ccbf97885b0e42955
BOSSLADY <gennymiriane06@gmail,com>
2025-11-20 18:24:56 +0100

    a guessing game

--- script.sh
+++ script.sh
@@ -3,7 +3,6 @@
 x=$(( 0 + RANDOM % (5 - 0 + 1) ))
 echo "=========================================="
 echo "  Guest a number to win between 0 to 5: "
-echo "=========================================="
  read -p "Enter the value you guest: " guess
 if [[ $y -eq $x ]]; then
   echo "You WIN"
