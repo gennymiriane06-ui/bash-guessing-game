8bab15cf4b0ba8a24ec3fa0e52bea64a41315ac4
BOSSLADY <gennymiriane06@gmail,com>
2025-11-21 12:08:46 +0100

    guessing game of numbers

--- script.sh
+++ script.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
-import random
 
-# Generate a random number between 1 and 100
-secret_number = random.randint(1, 100)
-attempts = 0
-guessed = False
+numb=50
+while (true); do
+read -r -p "Enter value guess: " guess
 
-print("Welcome to the Number Guessing Game!")
-print("I'm thinking of a number between 1 and 100.")
-print()
-
-# Main game loop using while
-while not guessed:
-    # Get user input
-    try:
-        guess = int(input("Enter your guess: "))
-        attempts += 1
-        
-        # Check the guess
-        if guess < secret_number:
-            print("Too low! Try again.")
-        elif guess > secret_number:
-            print("Too high! Try again.")
-        else:
-            guessed = True
-            print(f"\nCongratulations! You guessed it in {attempts} attempts!")
-            print(f"The number was {secret_number}")
+if [[ $guess -eq $numb ]]; then
+echo "that's correct"
+break
+elif [[ $guess -gt $numb ]]; then 
+echo "you loose try again"
+else
+echo "you loose try again"
+fi
+done
     
-    except ValueError:
-        print("Please enter a valid number!")
-
-print("\nThanks for playing!")
+    
