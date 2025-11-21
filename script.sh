0fbd0117d7bb57d2386494525d8b77d7f9fdc5ac
BOSSLADY <gennymiriane06@gmail,com>
2025-11-21 10:27:45 +0100

    a play game

--- script.sh
+++ script.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 secret_number=$((RANDOM % 100 + 1))
-guessed=false
-attempts=0
+guessed=0
 
 echo "Welcome to my Guessing Game!"
 echo "Guess a number 1 to 100."
 echo
 
-while [ "$y" -eq false ]; do
+while [ "$guessed" -eq 0" ]
 
     read -p "Enter your guess: " user_guess
     
-    ((attempts++))
     
- if [ "$y" -eq "$secret_number" ]; then
+ if [ "$guessed" $secret_number" ]; then
         echo " Congratulations! You guessed it!"
         echo "The number was $secret_number"
-        echo "It took you $attempts attempts."
-        y=true
- elif [ "$y" -lt "$secret_number" ]; then
+        guessed -eq true
+ elif [ "$guessed" -lt $secret_number ];then
      echo "Too low! Try again."
  else
      echo "Too high! Try again."
